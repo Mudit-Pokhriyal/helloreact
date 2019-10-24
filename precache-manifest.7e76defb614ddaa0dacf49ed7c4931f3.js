ef1cf3f5f981dfae9397f60739b86c1d71ae2bfa
Mudit <muditpokhriyal@protonmail.com>
2019-10-24 11:01:51 +0530

    Updates

---
+++ precache-manifest.7e76defb614ddaa0dacf49ed7c4931f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16e1072feb5d49f872261b931323c96",
+    "url": "/Mudit-Pokhriyal/helloreact/index.html"
+  },
+  {
+    "revision": "11483cc27542e9845181",
+    "url": "/Mudit-Pokhriyal/helloreact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7b4483add72901993f0c",
+    "url": "/Mudit-Pokhriyal/helloreact/static/js/2.4f33da63.chunk.js"
+  },
+  {
+    "revision": "11483cc27542e9845181",
+    "url": "/Mudit-Pokhriyal/helloreact/static/js/main.781d6d2e.chunk.js"
+  },
+  {
+    "revision": "fb856feda09af7971a40",
+    "url": "/Mudit-Pokhriyal/helloreact/static/js/runtime-main.b6da2e34.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Mudit-Pokhriyal/helloreact/static/media/logo.25bf045c.svg"
+  }
+]);
